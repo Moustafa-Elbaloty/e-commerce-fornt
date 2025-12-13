4cf57ec8d7d56041289f5b91bf616417574c96f2
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-12-13 21:49:28 +0200

    chore: clean up unused imports and optimize component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },      // الصفحة الرئيسية
+  { path: 'login', component: LoginComponent } // صفحة اللوجن
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
