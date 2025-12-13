01ea57395ec64df347ad3ee5fb5df80fe4ad2588
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-12-13 12:28:02 +0200

    Revert "Merge branch 'main' of https://github.com/Moustafa-Elbaloty/fornt-End"
    This reverts commit f453a448b865420ba5a89a62bd71f2c20b345d7e, reversing
    changes made to 41d78a36df10d0283daf391775402e385cc440e3.

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -15,4 +15,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
-
