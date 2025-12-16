71e642212bbca9d5584e9491273db9eaa58cd37a
Moustafa elbaloty <moustafaelbaloty@gmail.com>
2025-12-16 17:57:03 +0200

    fix: update logout method to redirect user after logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -29,7 +29,7 @@ export class NavbarComponent {
 
   logout() {
     this.auth.logout();
-  this.isUserMenuOpen = false; 
+  window.location.href = '/';
   }
 
   @HostListener('document:click', ['$event'])
