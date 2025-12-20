b5253a94a51bddbce143a1fefbca8428acf58e33
Mohamed saieed <mohamedsaieed216@gmail.com>
2025-12-20 04:20:54 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import { LoginComponent } from './pages/login/login.component';
+// Interceptor
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+
+// Components (Layout)
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { PromoComponent } from './components/promo/promo.component';
-import { FooterComponent } from './components/footer/footer.component';
+
+// Pages
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
@@ -25,17 +30,23 @@ import { MyordersComponent } from './pages/myorders/myorders.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { PaymentResultComponent } from './pages/payment-result/payment-result.component';
 
+// Admin
+import { AdminPanalComponent } from './components/admin-panal/admin-panal.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+
+    // layout
     NavbarComponent,
+    FooterComponent,
     HeroComponent,
-    ProductsComponent,
     CategoriesComponent,
     PromoComponent,
-    FooterComponent,
+
+    // pages
     HomeComponent,
+    LoginComponent,
     ProfileComponent,
     CartComponent,
     CheckoutComponent,
@@ -44,21 +55,25 @@ import { PaymentResultComponent } from './pages/payment-result/payment-result.co
     MyordersComponent,
     ProductDetailsComponent,
     PaymentResultComponent,
+
+    // admin
+    AdminPanalComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule, // ✅ يحل ngClass و number pipe
-    AppRoutingModule,
+    CommonModule,      // ngIf, ngFor, pipes
+    FormsModule,       // ngModel
     HttpClientModule,
-    FormsModule, // ✅ يحل ngModel
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true,
-    },
+      multi: true
+    }
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
